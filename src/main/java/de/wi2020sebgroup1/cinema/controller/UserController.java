6521eeca3372c8cba7b4c706ae9fd7be1d32d72b
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2021-11-29 15:04:00 +0100

    Fix tomcat 500 error

--- src/main/java/de/wi2020sebgroup1/cinema/controller/UserController.java
+++ src/main/java/de/wi2020sebgroup1/cinema/controller/UserController.java
@@ -1,62 +1,34 @@
 package de.wi2020sebgroup1.cinema.controller;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import de.wi2020sebgroup1.cinema.assembler.UserAssembler;
 import de.wi2020sebgroup1.cinema.exception.UserNotFoundException;
-import de.wi2020sebgroup1.cinema.model.User;
 import de.wi2020sebgroup1.cinema.repo.UserRepo;
 
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
 	
-	@Autowired
-	private final UserRepo REPOSITORY;
+	private final UserRepo userRepo;
 
-    private final UserAssembler ASSEMBLER;
-
-    public UserController(UserRepo REPOSITORY, UserAssembler ASSEMBLER) {
-        this.REPOSITORY = REPOSITORY;
-        this.ASSEMBLER = ASSEMBLER;
+    public UserController(UserRepo userRepo) {
+        this.userRepo = userRepo;
     }
-	
-	@GetMapping("/users")
-	public
-    ResponseEntity<?> all() {
 
-        List<EntityModel<User>> users = REPOSITORY.findAll().stream()
-                .map(ASSEMBLER::toModel)
-                .collect(Collectors.toList());
-
-        CollectionModel<EntityModel<User>> entityModels = CollectionModel.of(users, linkTo(methodOn(UserController.class).all()).withSelfRel());
-
-        return ResponseEntity.ok(entityModels);
+    @GetMapping("/users")
+    public ResponseEntity<?> all() {
+        return ResponseEntity.ok(userRepo.findAll());
     }
 
     @GetMapping("/users/{id}")
 	public
     ResponseEntity<?> one(@PathVariable long id) {
-
-    	User user = REPOSITORY.findById(id)
-                .orElseThrow(() -> new UserNotFoundException("Not found: "+id));
-
-        EntityModel<User> snackEntityModel = ASSEMBLER.toModel(user);
-
-        return ResponseEntity.ok(snackEntityModel);
+        return ResponseEntity.ok(userRepo.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("Not found: "+id)));
     }
 	
 }
--- src/main/java/de/wi2020sebgroup1/cinema/model/User.java
+++ src/main/java/de/wi2020sebgroup1/cinema/model/User.java
@@ -13,15 +13,16 @@ public class User {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "testid")
 	private long id;
 	
-	@Column(name = "first_name")
+	@Column(name = "testfirstname")
 	private String firstName;
 	
-	@Column(name = "last_name")
+	@Column(name = "testlastname")
 	private String lastName;
 	
-	@Column(name = "e-mail")
+	@Column(name = "testmail")
 	private String email;
 	
 	public User() {
@@ -57,6 +58,51 @@ public class User {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
 	}
 	
 }
