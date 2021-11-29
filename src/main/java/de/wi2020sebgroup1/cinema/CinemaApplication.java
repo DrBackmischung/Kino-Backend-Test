811ce51ef484c7c6d92245bda8977651da079b64
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2021-11-29 10:40:34 +0100

    create basic structure

--- src/main/java/de/wi2020sebgroup1/cinema/CinemaApplication.java
+++ src/main/java/de/wi2020sebgroup1/cinema/CinemaApplication.java
@@ -4,23 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import de.wi2020sebgroup1.cinema.model.User;
 import de.wi2020sebgroup1.cinema.repo.UserRepo;
 
 @SpringBootApplication
-public class CinemaApplication implements CommandLineRunner {
+public class CinemaApplication
+//implements CommandLineRunner
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaApplication.class, args);
 	}
 	
-	@Autowired
-	private UserRepo userRepo;
-
-	@Override
-	public void run(String... args) throws Exception {
-		this.userRepo.save(new User("Mathis", "Neunzig", "mathis.neunzig@gmail.com"));
-	}
+//	@Autowired
+//	private UserRepo userRepo;
+//	
+//	@Autowired
+//	private JdbcTemplate db;
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		this.userRepo.save(new User("Mathis", "Neunzig", "mathis.neunzig@gmail.com"));
+//	}
 
 }
--- src/main/java/de/wi2020sebgroup1/cinema/controller/UserController.java
+++ src/main/java/de/wi2020sebgroup1/cinema/controller/UserController.java
@@ -1,23 +1,62 @@
 package de.wi2020sebgroup1.cinema.controller;
 
-import java.util.List;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import de.wi2020sebgroup1.cinema.assembler.UserAssembler;
+import de.wi2020sebgroup1.cinema.exception.UserNotFoundException;
 import de.wi2020sebgroup1.cinema.model.User;
 import de.wi2020sebgroup1.cinema.repo.UserRepo;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("/api/v1")
 public class UserController {
 	
-	private UserRepo userRepo;
+	@Autowired
+	private final UserRepo REPOSITORY;
+
+    private final UserAssembler ASSEMBLER;
+
+    public UserController(UserRepo REPOSITORY, UserAssembler ASSEMBLER) {
+        this.REPOSITORY = REPOSITORY;
+        this.ASSEMBLER = ASSEMBLER;
+    }
 	
-	@GetMapping("users")
-	public List<User> getUsers() {
-		return this.userRepo.findAll();
-	}
+	@GetMapping("/users")
+	public
+    ResponseEntity<?> all() {
+
+        List<EntityModel<User>> users = REPOSITORY.findAll().stream()
+                .map(ASSEMBLER::toModel)
+                .collect(Collectors.toList());
+
+        CollectionModel<EntityModel<User>> entityModels = CollectionModel.of(users, linkTo(methodOn(UserController.class).all()).withSelfRel());
+
+        return ResponseEntity.ok(entityModels);
+    }
+
+    @GetMapping("/users/{id}")
+	public
+    ResponseEntity<?> one(@PathVariable long id) {
+
+    	User user = REPOSITORY.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("Not found: "+id));
+
+        EntityModel<User> snackEntityModel = ASSEMBLER.toModel(user);
+
+        return ResponseEntity.ok(snackEntityModel);
+    }
 	
 }
