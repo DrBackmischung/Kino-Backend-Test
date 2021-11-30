d64f2aa3299c71ec2d63b2226bce8cbf47dc68b6
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2021-11-30 09:36:30 +0100

    test security

--- src/main/java/de/wi2020sebgroup1/cinema/SecurityConfig.java
+++ src/main/java/de/wi2020sebgroup1/cinema/SecurityConfig.java
@@ -1,16 +1,17 @@
 package de.wi2020sebgroup1.cinema;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+    @Override
+    protected void configure(HttpSecurity security) throws Exception
+    {
+     security.httpBasic().disable();
     }
 }
